7c6d00c0a61f11602ea292e2f6d16e60a85f7a27
Gene Harrel <generharrel@gmail.com>
2020-05-02 08:16:12 -0700

    Redux Adding Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -5,12 +5,18 @@ import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    postFavorite: campsiteId => (postFavorite(campsiteId))
 };
 
 function RenderCampsite(props) {
@@ -74,9 +80,9 @@ class CampsiteInfo extends Component {
         };
     }
 
-    markFavorite() {
-        this.setState({favorite: true});
-    }    
+    markFavorite(campsiteId) {
+        this.props.postFavorite(campsiteId);
+    }   
 
     static navigationOptions = {
         title: 'Campsite Information'
@@ -89,8 +95,8 @@ class CampsiteInfo extends Component {
         return (
             <ScrollView>
                 <RenderCampsite campsite={campsite}
-                    favorite={this.state.favorite}
-                    markFavorite={() => this.markFavorite()}
+                    favorite={this.props.favorites.includes(campsiteId)}
+                    markFavorite={() => this.markFavorite(campsiteId)}
                 />
                 <RenderComments comments={comments} />
             </ScrollView>  
@@ -98,4 +104,4 @@ class CampsiteInfo extends Component {
     }
 }
 
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
