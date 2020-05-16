b2ae1855bac6532ff4938f05b1e3cc802cc1bc41
Gene Harrel <generharrel@gmail.com>
2020-05-16 13:44:00 -0700

    Week 4 Workshop

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,8 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from "expo-image-manipulator";
+import * as MediaLibrary from 'expo-media-library';
 
 class LoginTab extends Component {
 
@@ -154,7 +156,40 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
+    processImage = async (imgUri) => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [{resize: { width: 400, height: 400 }}],
+            { compress: 1, format: 'png' }
+          );
+
+        console.log(processedImage);
+
+        this.setState({imageUrl: processedImage.uri});
+
+        if (cameraRollPermission.status === 'granted') {
+            MediaLibrary.saveToLibraryAsync(processedImage.uri);
+        }
+    }
+
+
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1],
+              });
+        if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
             }
         }
     }
@@ -184,6 +219,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
